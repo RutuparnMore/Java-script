13f95b8efcb07929170ee0b8e4871ac44927a56a
RutuparnMore <rutuparn8888@gmail.com>
2025-04-17 14:50:36 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+ let score = "rutu"
+
+ //console.log(typeof score);
+ //console.log(typeof(score));
+
+let valueInNuber = Number(score)
+//console.log(typeof valueInNuber);
+//console.log(valueInNuber);
+
+
+//"33 =>33
+// "33abc" => NaN
+// true => 1
+// false =>0
+
+let isLoggedIn = "rutu"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// ""=> false
+//"rutu" => true
+
+let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
