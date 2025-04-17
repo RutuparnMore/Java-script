9394a5e005b5a28f359a613962283d531444bf19
RutuparnMore <rutuparn8888@gmail.com>
2025-04-17 14:09:49 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "rutuparn8888@gmai.com"
+var accountPassword= "12345"
+accountcity = "jaipur"
+
+//accountId = 2 not allowed
+
+accountEmail = "rm@rm.com"
+paccountPassword =935649
+accountCity = "mangrulpir"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
