ec536fce155dbde85df04f72668435f0f9d74108
RutuparnMore <rutuparn8888@gmail.com>
2025-04-17 14:09:19 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3
+    +3) // don't do this ,code readability is priority
+
+console.log("rutu")
+
+
+
+let name = "rutuparn"
+let age = 18
+let isloggedin = false
+
+
+// number => 2 to the power 53
+// bigint
+//string => ""
+// boolean => true/false
+//null => standalone value
+// undifined =>
+// symbols => unique
+
+
+// object 
+console.log(typeof undefined); // undefiend
+console.log(typeof null);// object
+
+
